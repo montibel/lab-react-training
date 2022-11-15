06119d1ffa10512161cba97978baa5b68b049d4b
montibel <113550254+montibel@users.noreply.github.com>
2022-11-15 21:50:45 +0100

    it4done

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,4 +1,4 @@
-import React, { Children } from "react"; 
+import React from "react"; 
 
 function Greetings ({lang, children}) {
     const hello = {
--- src/components/Random.js
+++ src/components/Random.js
@@ -1,4 +1,4 @@
-import React, { Children } from "react"; 
+import React from "react"; 
 
 function Random ({min, max}) {
    const randomNumber = Math.floor(Math.random()* max);
